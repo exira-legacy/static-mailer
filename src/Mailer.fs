ee6c5b441d53c4d1c819c26f015807d164f0404c
David Cumps <david@cumps.be>
2015-11-16 16:21:32 +0100

    Provide mail template

--- src/Mailer.fs
+++ src/Mailer.fs
@@ -24,11 +24,43 @@ mailerConfig.Load configPath
 let sendStaticLogo =
     sendResource entryAssembly "index.html" true
 
-let greetings form =
-  defaultArg (Option.ofChoice(form ^^ "name")) "World"
-  |> sprintf "Hello %s"
+let buildSubject form =
+    let template = mailerConfig.Mailer.Subject
+    let site = defaultArg (Option.ofChoice(form ^^ "site")) "static-mailer"
+    template.Replace("%SITE%", site)
+
+let buildBody form =
+    let template = mailerConfig.Mailer.Template
+
+    let site = defaultArg (Option.ofChoice(form ^^ "site")) "static-mailer"
+    let name = defaultArg (Option.ofChoice(form ^^ "name")) "N/A"
+    let email = defaultArg (Option.ofChoice(form ^^ "email")) "N/A"
+    let subject = defaultArg (Option.ofChoice(form ^^ "subject")) "N/A"
+    let message = defaultArg (Option.ofChoice(form ^^ "message")) "N/A"
+
+    template
+        .Replace("%SITE%", site)
+        .Replace("%NAME%", name)
+        .Replace("%EMAIL%", email)
+        .Replace("%SUBJECT%", subject)
+        .Replace("%MESSAGE%", message)
+
+let sendMail subject body =
+    let mailTo = mailerConfig.Mailer.To
+    // TODO: Send HTML mail
+    ()
+
+let contact form =
+    let subject = buildSubject form
+    let body = buildBody form
+
+    try
+        sendMail subject body
+        sprintf "success"
+    with
+    | ex -> sprintf "fail"
 
 let application =
   choose
-    [ POST >>= path "/send" >>= request (fun request -> OK <| greetings request.form)
+    [ POST >>= path "/send" >>= request (fun request -> OK <| contact request.form)
       sendStaticLogo ]
