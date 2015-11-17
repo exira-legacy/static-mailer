72fdebf37bafc48ea7277ee4a6b2a758df5c3b3d
David Cumps <david@cumps.be>
2015-11-17 23:04:58 +0100

    Update Program.fs

--- src/Program.fs
+++ src/Program.fs
@@ -28,7 +28,6 @@ let start hostControl =
     let httpsBinding =
         use bio = new BIO(File.ReadAllBytes mailerConfig.Mailer.Endpoint.HttpsPfx)
         let cert = X509Certificate.FromPKCS12(bio, mailerConfig.Mailer.Endpoint.HttpsPassword)
-
         HttpBinding.mk (HTTPS (open_ssl cert)) (IPAddress.Parse "0.0.0.0") (uint16 mailerConfig.Mailer.Endpoint.HttpsPort)
 
     let bindings =
