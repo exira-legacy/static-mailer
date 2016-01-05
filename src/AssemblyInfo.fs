8abc7e30acf14fc3640f27e0076583bf55704450
Team City <info@exira.com>
2016-01-05 20:33:31 +0000

    Bump version to 1.2.21

--- src/AssemblyInfo.fs
+++ src/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("static-mailer")>]
 [<assembly: AssemblyProductAttribute("Exira.StaticMailer")>]
 [<assembly: AssemblyDescriptionAttribute("Exira.StaticMailer is a REST endpoint running in a Windows Service to enable static sites to easily send mail")>]
-[<assembly: AssemblyVersionAttribute("1.2.20")>]
-[<assembly: AssemblyFileVersionAttribute("1.2.20")>]
-[<assembly: AssemblyMetadataAttribute("githash","3d16adab3401d5e4f7e2925e7127460c60c02966")>]
+[<assembly: AssemblyVersionAttribute("1.2.21")>]
+[<assembly: AssemblyFileVersionAttribute("1.2.21")>]
+[<assembly: AssemblyMetadataAttribute("githash","46e4fac85bb5e37f74ea7830415e5c17c5eb9444")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.2.20"
+    let [<Literal>] Version = "1.2.21"
